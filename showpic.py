1aa1dbbfcd2f47b3ad648a8d93d10ee3c282aa04
ksy <910661511@qq.com>
2018-12-06 15:14:35 +0800

    优化了图片显示时的空白部分，增加了每个buffer是否显示的设置（被某些狂发图片的群烦死）

--- showpic.py
+++ showpic.py
@@ -28,16 +28,19 @@
 import weechat as w
 import requests
 import re
+from PIL import Image
+import json
 
 SCRIPT_NAME    = "showpic"
 SCRIPT_AUTHOR  = "ksy <910661511@qq.com>"
-SCRIPT_VERSION = "0.5"
+SCRIPT_VERSION = "0.6"
 SCRIPT_LICENSE = "GPL3"
 SCRIPT_DESC    = "show the pictures in buffer"
 SCRIPT_COMMAND  = SCRIPT_NAME
 
 settings = {
     'switch': 'on',  
+    'exclude_list':'[]',
     'showall': 'on',
     'showtime': '3',
 }
@@ -51,33 +54,57 @@ if w.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE,SCRIPT_
         "show the pictures in buffer",
         "[on/off]",
         "switch the mode \n",
-        " || on|off"
+        " || on|off|exclude|include"
         " || showtime"
         " || showall on|off",
         "showpic_cb",
         '')
 
+    showtime = int(w.config_get_plugin('showtime'))
+    lastpic = {}
+    exclude_list=json.loads(w.config_get_plugin('exclude_list'))
+
+    w.hook_config("plugins.var.python." + SCRIPT_NAME + ".switch", "config_cb", "")
+    hook = w.hook_print("", "", "", 1, "my_print_cb", "")
+
+def show_pic(pic, size, x, y):
+    global showtime
+    im = Image.open(pic)
+    ww = im.size[0]
+    hh = im.size[1]
+    if ww >= hh:
+        hh = hh * size / ww
+        ww = size
+    else:
+        ww = ww * size / hh
+        hh = size
+    cmd = "feh -g %sx%s+%s+%s --scale-down " % (ww, hh, str(x), str(y))
+    w.hook_process(cmd + pic, showtime * 1000, "", "")
+
 def my_print_cb(data, buffer, date, tags, displayed, highlight, prefix, message):
     showall = True if w.config_get_plugin('showall') == 'on' else False
-    global lastpic
+    global lastpic, exclude_list
     if highlight or showall:
         msg = message
-        showtime = int(w.config_get_plugin('showtime'))
         if msg.startswith('[图片]('):
             pic = msg.lstrip('[图片](').rstrip(')')
             lastpic[buffer] = pic
-            w.hook_process("feh -g 800x600+1000+400 --scale-down " + pic, showtime * 1000, "", "")
+            if buffer not in exclude_list:
+                show_pic(pic, 500, 1300, 600)
         elif msg.startswith('[表情]('):
             pic = msg.lstrip('[表情](').rstrip(')')
-            w.hook_process("feh -g 200x200+1000+400 --scale-down " + pic, showtime * 1000, "", "")
+            if buffer not in exclude_list:
+                show_pic(pic, 200, 1000, 400)
         elif 'https://img.vim-cn.com/' in msg:
             msg = re.findall(r"(https:.*?(png|jpg|jpeg|gif))", msg)[0][0]
             r = requests.get(msg, stream=True)
-            with open('/tmp/picbed_tmp.png','wb') as f:
+            pic = '/tmp/picbed_tmp.png'
+            with open(pic,'wb') as f:
                 for chunk in r.iter_content(chunk_size=32):
                     f.write(chunk)
-            w.hook_process("feh -g 800x600+1000+400 --scale-down " + '/tmp/picbed_tmp.png', showtime * 1000, "", "")
-            lastpic[buffer] = '/tmp/picbed_tmp.png'
+            lastpic[buffer] = pic
+            if buffer not in exclude_list:
+                show_pic(pic, 500, 1300, 600)
     return w.WEECHAT_RC_OK
 
 def config_cb(data, option, value):
@@ -90,14 +117,25 @@ def config_cb(data, option, value):
     return w.WEECHAT_RC_OK
 
 def showpic_cb(data, buffer, args):
+    global showtime, exclude_list
     if args == '':
-        w.hook_process("feh -g 800x600+1000+400 --scale-down " + lastpic[buffer], 0, "", "")
+        show_pic(lastpic[buffer],500, 1300, 600)
     else:
         args = args.split()
         if args[0] == 'on':
             w.config_set_plugin('switch', 'on')
         elif args[0] == 'off':
             w.config_set_plugin('switch', 'off')
+        elif args[0] == 'exclude':
+            if buffer not in exclude_list:
+                exclude_list.append(buffer)
+                w.config_set_plugin('exclude_list', json.dumps(exclude_list))
+            w.prnt(buffer, "pic won't show on this buffer")
+        elif args[0] == 'include':
+            if buffer in exclude_list:
+                exclude_list.remove(buffer)
+                w.config_set_plugin('exclude_list', json.dumps(exclude_list))
+            w.prnt(buffer, "pic will show on this buffer again")
         elif args[0] == 'showall':
             if args[1] == 'on':
                 w.config_set_plugin('showall', 'on')
@@ -105,9 +143,7 @@ def showpic_cb(data, buffer, args):
                 w.config_set_plugin('showall', 'off')
         elif args[0] == 'showtime':
             if args[1].isdigit():
+                showtime = args[1]
                 w.config_set_plugin('showtime', args[1])
     return w.WEECHAT_RC_OK
 
-hook = w.hook_print("", "", "", 1, "my_print_cb", "")
-w.hook_config("plugins.var.python." + SCRIPT_NAME + ".switch", "config_cb", "")
-lastpic = {}
